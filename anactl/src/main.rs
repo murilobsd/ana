22ece28bd96d62b16f1cb387e3d4b8086e029d8e
murilo ijanc <mbsd@m0x.ru>
2024-09-30 15:14:08 -0300

    chore: simple control and server run

--- anactl/src/main.rs
+++ anactl/src/main.rs
@@ -1,3 +1,75 @@
+// Copyright (c) 2024 Murilo Ijanc' <mbsd@m0x.ru>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+use std::io::{self, Read, Write};
+use std::os::unix::net::UnixStream;
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel::<String>();
+
+    thread::spawn(move || {
+        let socket_path = "/tmp/anad.sock";
+
+        let mut stream = match UnixStream::connect(socket_path) {
+            Ok(stream) => stream,
+            Err(e) => {
+                eprintln!("Failed to connect to UnixStream: {}", e);
+                return;
+            }
+        };
+
+        for command in rx {
+            if let Err(e) = stream.write_all(command.as_bytes()) {
+                eprintln!("Failed to send to command via UnixStream: {}", e);
+            } else {
+                let mut buffer = [0; 1024];
+                match stream.read(&mut buffer) {
+                    Ok(n) if n > 0 => {
+                        let response = String::from_utf8_lossy(&buffer[..n]);
+                        println!("Response from server: {}", response);
+                    }
+                    Ok(_) => println!("No response received"),
+                    Err(e) => {
+                        eprintln!("Failed to read from UnixStream: {}", e)
+                    }
+                }
+            }
+        }
+    });
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let input = input.trim();
+        if input.is_empty() {
+            continue;
+        }
+
+        if let Err(e) = tx.send(input.to_string()) {
+            eprintln!("Failed to send command to thread: {}", e);
+            break;
+        }
+
+        if input == "/quit" {
+            println!("Quiting...");
+            break;
+        }
+    }
 }
--- anad/src/main.rs
+++ anad/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: UnixStream) {
+    let mut buffer = [0; 1024];
+
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(n) if n > 0 => {
+                let received = String::from_utf8_lossy(&buffer[..n]);
+                println!("Received command: {}", received);
+                
+                // Responder ao comando
+                let response = format!("Command '{}' received!", received.trim());
+                if let Err(e) = stream.write_all(response.as_bytes()) {
+                    eprintln!("Failed to send response: {}", e);
+                }
+            }
+            Ok(_) => break,
+            Err(e) => {
+                eprintln!("Failed to read from stream: {}", e);
+                break;
+            }
+        }
+    }
 }
+
+fn main() -> std::io::Result<()> {
+    let socket_path = "/tmp/anad.sock";
+    let listener = UnixListener::bind(socket_path)?;
+
+    println!("Server listening on {}", socket_path);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Client connected!");
+                handle_client(stream);
+            }
+            Err(err) => eprintln!("Connection failed: {}", err),
+        }
+    }
+
+    Ok(())
+}
+
